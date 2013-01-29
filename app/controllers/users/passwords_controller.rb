20023577709bdf78d4f1c5a047309da93085b869
Aaron Kaczmarek <aaronmichaelmusic@gmail.com>
2013-01-29 13:48:21 -0500

    devise slugs

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -4,12 +4,12 @@ class Users::PasswordsController < Devise::PasswordsController
   
   def initialize_page
     if params[:action] == "new"
-      @page = Page.find_by_permalink('users/password/new')
+      @page = Page.find_by_slug('forgot-password')
       @page.page_parts.each do |pp|
         instance_variable_set "@#{pp.title.gsub(' ', '_').downcase}", @page.page_part_placements.find_by_page_part_id(pp.id)
       end
     elsif params[:action] == "edit"
-      @page = Page.find_by_permalink('users/password/edit')
+      @page = Page.find_by_slug('change-password')
       @page.page_parts.each do |pp|
         instance_variable_set "@#{pp.title.gsub(' ', '_').downcase}", @page.page_part_placements.find_by_page_part_id(pp.id)
       end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,7 @@ class Users::SessionsController < Devise::SessionsController
   before_filter :initialize_page
   
   def initialize_page
-    @page = Page.find_by_permalink('users/sign_in')
+    @page = Page.find_by_slug('login')
     @page.page_parts.each do |pp|
       instance_variable_set "@#{pp.title.gsub(' ', '_').downcase}", @page.page_part_placements.find_by_page_part_id(pp.id)
     end
