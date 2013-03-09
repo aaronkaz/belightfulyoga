05986bfd7a61495e8a266aab829d188e7bbe4640
Aaron Kaczmarek <aaronmichaelmusic@gmail.com>
2013-03-09 16:56:23 -0500

    migrate waivers

--- app/models/course_registration.rb
+++ app/models/course_registration.rb
@@ -8,6 +8,15 @@ class CourseRegistration < ActiveRecord::Base
   
   after_create :send_confirmation_email
   
+  
+  def waiver_file
+    self.cart.present? ? "/waivers/#{self.cart.session_id}.waiver.#{self.course.old_id}.html" : nil
+  end
+  
+  def has_waiver_file?
+    !self.waiver_file.nil? && File.exists?("#{Rails.root}/public#{self.waiver_file}") ? true : false
+  end
+  
   RailsAdmin.config do |config|
     config.model CourseRegistration do    
       navigation_label 'Course Management'
@@ -18,7 +27,15 @@ class CourseRegistration < ActiveRecord::Base
         field :course
         field :user
         field :cart
-        field :waiver
+        field :waiver do
+          pretty_value do
+            if bindings[:object].waiver.present?
+              bindings[:view].link_to("Waiver #{bindings[:object].waiver.present?}", bindings[:object].waiver)
+            else
+              bindings[:object].has_waiver_file? ? bindings[:view].link_to("Waiver", "#{bindings[:object].waiver_file}") : nil
+            end  
+          end
+        end
       end
       
     end
