e672542e16bba2ca3a4cccc429c4ac55b6d52bd0
aaronkaz <aaronmichaelmusic@gmail.com>
2014-10-06 12:53:05 -0400

    fix events without end date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,8 @@ class EventsController < ApplicationController
   before_filter :initialize_page
   
   def index
-    @events = Event.where('start_date is null OR end_date >= ?', Date.today).order(:start_date)
+    @events = Event.where('(start_date IS NOT NULL AND end_date IS NULL AND start_date >= ?) OR (start_date IS NOT NULL AND end_date IS NOT NULL AND end_date >= ?) OR start_date IS NULL', Date.today, Date.today).order(:start_date)
+    
   end
   
 private
