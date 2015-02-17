e354ef3b0a9d0655eca9bf318ef3e5026a1833ad
aaronkaz <aaronmichaelmusic@gmail.com>
2015-02-17 15:57:49 -0500

    trainings

--- app/uploaders/course_image_uploader.rb
+++ app/uploaders/course_image_uploader.rb
@@ -18,7 +18,7 @@ class CourseImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "#{model.class.to_s.underscore}"
+    "course"
   end
 
   process :resize_to_limit => [400, 400]
