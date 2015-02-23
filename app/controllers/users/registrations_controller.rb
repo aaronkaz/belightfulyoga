f02fa116677d0756306fe6c4078ef146d17c9dfb
aaronkaz <aaronmichaelmusic@gmail.com>
2015-02-23 13:20:09 -0500

    sessions and registration show in template, responsive home video

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,21 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   layout 'user', :only => [:edit]
-  before_filter :initialize_page
-  
-  def initialize_page
-    if params[:action] == "new"
-      @page = Page.find_by_slug('register')
-      @page.page_parts.each do |pp|
-        instance_variable_set "@#{pp.title.gsub(' ', '_').downcase}", @page.page_part_placements.find_by_page_part_id(pp.id)
-      end
-    elsif params[:action] == "edit"
-      @page = Page.find_by_slug('edit-account')
-      @page.page_parts.each do |pp|
-        instance_variable_set "@#{pp.title.gsub(' ', '_').downcase}", @page.page_part_placements.find_by_page_part_id(pp.id)
-      end      
-      @sidebar_override = render_to_string(:partial => 'layouts/account')
-    end  
-  rescue
-  end
+
   
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,13 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
   layout 'application' 
-  before_filter :initialize_page
-  
-  def initialize_page
-    @page = Page.find_by_slug('login')
-    @page.page_parts.each do |pp|
-      instance_variable_set "@#{pp.title.gsub(' ', '_').downcase}", @page.page_part_placements.find_by_page_part_id(pp.id)
-    end
-  rescue
-  end
+
     
 end
